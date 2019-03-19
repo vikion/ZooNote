0f3115590fcdc0e855045785a6351a034cc4d0bd
Martin <pilat@gmail.com>
2019-03-19 20:17:35 +0100

    pridany logout

--- src/sample/contentController.java
+++ src/sample/contentController.java
@@ -1,14 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class contentController implements Initializable {
+public class contentController extends loginController implements Initializable {
 
     @FXML
     BorderPane mainPane;
@@ -54,6 +59,15 @@ public class contentController implements Initializable {
     public void onUsersBtnClick(){
         Users users = new Users();
         mainPane.setCenter(users);
+    }
+
+    @FXML
+    public void onLogoutBtnClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("layout/sample.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) menoPriezvisko.getScene().getWindow();
+        stage.setScene(scene);
     }
 
 
